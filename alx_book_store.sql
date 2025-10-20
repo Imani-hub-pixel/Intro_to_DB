1b77fc5126f8e283ac89b4402a95682ceb6fd0dd
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-10-21 01:16:49 +0300

    DB

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -24,14 +24,14 @@ CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+     FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
 );
 CREATE TABLE Order_Details(
     orderdetilid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY(order_id) REFERENCES orders(order_id),
-	FOREIGN KEY(book_id) REFERENCES Books(book_id)
+     FOREIGN KEY(order_id) REFERENCES orders(order_id),
+	 FOREIGN KEY(book_id) REFERENCES Books(book_id)
     
 );
